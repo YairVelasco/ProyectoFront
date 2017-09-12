4bf127f1b06991850c4d803583679eb62a8a0fe0
YairVelasco <30699910+YairVelasco@users.noreply.github.com>
2017-09-12 15:52:24 -0500

    Styles
    Added information from the API, and worked on styles

--- www/javascript/main.js
+++ www/javascript/main.js
@@ -7,7 +7,13 @@ $(function(){
     success: function(movies){
       $.each(movies, function(i,movie){
         //$movies.append('<li>Nombre' +movie.name+ '</li>','<li>Nombre' +movie.image.medium+ '</li>' );
-        $movies.append(`<li><h3 id="name">${movie.name}</h3><div><img src= "${movie.image.medium}"></div></li>`);
+        $movies.append(`<li><p class="name">${movie.name}</p>
+        <div>
+          <a href= "${movie.officialSite}"><img src= "${movie.image.medium}"></a>
+        </div>
+        <div class= "info">Date: ${movie.premiered}<br>Rating: ★${movie.rating.average}</div>
+        <div class= "summ">${movie.summary}</div>
+        `);
       });
     }
   });
