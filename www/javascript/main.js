2125f8ac9321cc24bfefbb34d400163df42dfdd7
YairVelasco <30699910+YairVelasco@users.noreply.github.com>
2017-09-12 12:59:40 -0500

    FlexWrap

--- www/javascript/main.js
+++ www/javascript/main.js
@@ -6,7 +6,8 @@ $(function(){
     url: 'http://api.tvmaze.com/shows',
     success: function(movies){
       $.each(movies, function(i,movie){
-        $movies.append('<li>Nombre' +movie.name+ '</li>');
+        //$movies.append('<li>Nombre' +movie.name+ '</li>','<li>Nombre' +movie.image.medium+ '</li>' );
+        $movies.append(`<li><h3 id="name">${movie.name}</h3><div><img src= "${movie.image.medium}"></div></li>`);
       });
     }
   });
