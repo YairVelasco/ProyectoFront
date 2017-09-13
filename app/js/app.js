5babced584d49852db1cc14c33b4aa5862bab204
YairVelasco <30699910+YairVelasco@users.noreply.github.com>
2017-09-13 18:03:49 -0500

    Kendo
    Added kendo features and a new view with it

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,22 @@
 
-var $movies = $('#movies');
+var $movies = $('#movies'),
+    dataAjax = "";
 $(function(){
     $.ajax({
     type: 'GET',
     url: 'http://api.tvmaze.com/shows',
     success: function(movies){
       $.each(movies, function(i,movie){
+        dataAjax = movies;
+        var movieTime = new Date(movie.premiered+" "+movie.schedule.time);
+        console.log(movieTime.getHours()+":"+(movieTime.getMinutes()+30));
         //$movies.append('<li>Nombre' +movie.name+ '</li>','<li>Nombre' +movie.image.medium+ '</li>' );
         $movies.append(`<li><p class="name">${movie.name}</p>
         <div>
           <a href= "${movie.officialSite}"><img src= "${movie.image.medium}"></a>
         </div>
         <div class= "info">Date: ${movie.premiered}<br>Rating: ★${movie.rating.average}</div>
-        <div class= "summ">${movie.summary}</div>
+        <div class= "summ">${movieTime}</div>
         `);
       });
     }
--- www/js/app.js
+++ www/js/app.js
@@ -1,14 +1,18 @@
 'use strict';
 
-var $movies = $('#movies');
+var $movies = $('#movies'),
+    dataAjax = "";
 $(function () {
   $.ajax({
     type: 'GET',
     url: 'http://api.tvmaze.com/shows',
     success: function success(movies) {
       $.each(movies, function (i, movie) {
+        dataAjax = movies;
+        var movieTime = new Date(movie.premiered + " " + movie.schedule.time);
+        console.log(movieTime.getHours() + ":" + (movieTime.getMinutes() + 30));
         //$movies.append('<li>Nombre' +movie.name+ '</li>','<li>Nombre' +movie.image.medium+ '</li>' );
-        $movies.append('<li><p class="name">' + movie.name + '</p>\n        <div>\n          <a href= "' + movie.officialSite + '"><img src= "' + movie.image.medium + '"></a>\n        </div>\n        <div class= "info">Date: ' + movie.premiered + '<br>Rating: \u2605' + movie.rating.average + '</div>\n        <div class= "summ">' + movie.summary + '</div>\n        ');
+        $movies.append('<li><p class="name">' + movie.name + '</p>\n        <div>\n          <a href= "' + movie.officialSite + '"><img src= "' + movie.image.medium + '"></a>\n        </div>\n        <div class= "info">Date: ' + movie.premiered + '<br>Rating: \u2605' + movie.rating.average + '</div>\n        <div class= "summ">' + movieTime + '</div>\n        ');
       });
     }
   });
